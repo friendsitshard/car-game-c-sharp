fd11fb940613d0ff681df90f5add83d46740ac73
friendsitshard <tsiklauri2016@mail.ru>
2024-01-11 02:02:03 +0400

    Added main gameplay

---
+++ Fasr-n-Furious/GameForm.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Fasr_n_Furious
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.EnemyPictureBox = new System.Windows.Forms.PictureBox();
+            this.CarPictureBox = new System.Windows.Forms.PictureBox();
+            this.MapPictureBox = new System.Windows.Forms.PictureBox();
+            this.MapPictureBox2 = new System.Windows.Forms.PictureBox();
+            this.EnemyPictureBox2 = new System.Windows.Forms.PictureBox();
+            this.LosePanel = new System.Windows.Forms.Panel();
+            this.LoseLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RestartButton = new System.Windows.Forms.Button();
+            this.HomeButton = new System.Windows.Forms.Button();
+            this.PointsLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Points = new System.Windows.Forms.Label();
+            this.CurrentPointsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CarPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MapPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MapPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyPictureBox2)).BeginInit();
+            this.LosePanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // EnemyPictureBox
+            // 
+            this.EnemyPictureBox.Image = global::Fasr_n_Furious.Properties.Resources.enemy_car;
+            this.EnemyPictureBox.Location = new System.Drawing.Point(199, -150);
+            this.EnemyPictureBox.Name = "EnemyPictureBox";
+            this.EnemyPictureBox.Size = new System.Drawing.Size(100, 130);
+            this.EnemyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.EnemyPictureBox.TabIndex = 3;
+            this.EnemyPictureBox.TabStop = false;
+            // 
+            // CarPictureBox
+            // 
+            this.CarPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.CarPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("CarPictureBox.Image")));
+            this.CarPictureBox.Location = new System.Drawing.Point(436, 500);
+            this.CarPictureBox.Name = "CarPictureBox";
+            this.CarPictureBox.Size = new System.Drawing.Size(100, 130);
+            this.CarPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CarPictureBox.TabIndex = 1;
+            this.CarPictureBox.TabStop = false;
+            // 
+            // MapPictureBox
+            // 
+            this.MapPictureBox.Image = global::Fasr_n_Furious.Properties.Resources.bg;
+            this.MapPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.MapPictureBox.Name = "MapPictureBox";
+            this.MapPictureBox.Size = new System.Drawing.Size(840, 650);
+            this.MapPictureBox.TabIndex = 0;
+            this.MapPictureBox.TabStop = false;
+            // 
+            // MapPictureBox2
+            // 
+            this.MapPictureBox2.Image = global::Fasr_n_Furious.Properties.Resources.bg;
+            this.MapPictureBox2.Location = new System.Drawing.Point(0, -650);
+            this.MapPictureBox2.Name = "MapPictureBox2";
+            this.MapPictureBox2.Size = new System.Drawing.Size(840, 650);
+            this.MapPictureBox2.TabIndex = 2;
+            this.MapPictureBox2.TabStop = false;
+            // 
+            // EnemyPictureBox2
+            // 
+            this.EnemyPictureBox2.Image = global::Fasr_n_Furious.Properties.Resources.enemy_car;
+            this.EnemyPictureBox2.Location = new System.Drawing.Point(544, -400);
+            this.EnemyPictureBox2.Name = "EnemyPictureBox2";
+            this.EnemyPictureBox2.Size = new System.Drawing.Size(100, 130);
+            this.EnemyPictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.EnemyPictureBox2.TabIndex = 4;
+            this.EnemyPictureBox2.TabStop = false;
+            // 
+            // LosePanel
+            // 
+            this.LosePanel.BackColor = System.Drawing.Color.White;
+            this.LosePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LosePanel.Controls.Add(this.CurrentPointsLabel);
+            this.LosePanel.Controls.Add(this.HomeButton);
+            this.LosePanel.Controls.Add(this.RestartButton);
+            this.LosePanel.Controls.Add(this.label2);
+            this.LosePanel.Controls.Add(this.label1);
+            this.LosePanel.Controls.Add(this.LoseLabel);
+            this.LosePanel.Location = new System.Drawing.Point(270, 166);
+            this.LosePanel.Name = "LosePanel";
+            this.LosePanel.Size = new System.Drawing.Size(304, 242);
+            this.LosePanel.TabIndex = 5;
+            this.LosePanel.Visible = false;
+            // 
+            // LoseLabel
+            // 
+            this.LoseLabel.AutoSize = true;
+            this.LoseLabel.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoseLabel.Location = new System.Drawing.Point(88, 32);
+            this.LoseLabel.Name = "LoseLabel";
+            this.LoseLabel.Size = new System.Drawing.Size(117, 29);
+            this.LoseLabel.TabIndex = 0;
+            this.LoseLabel.Text = "You Lose!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(30, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(32, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Highscore:";
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Location = new System.Drawing.Point(109, 160);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(75, 23);
+            this.RestartButton.TabIndex = 3;
+            this.RestartButton.Text = "Restart";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // HomeButton
+            // 
+            this.HomeButton.Location = new System.Drawing.Point(109, 195);
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Size = new System.Drawing.Size(75, 23);
+            this.HomeButton.TabIndex = 4;
+            this.HomeButton.Text = "Home";
+            this.HomeButton.UseVisualStyleBackColor = true;
+            // 
+            // PointsLabel
+            // 
+            this.PointsLabel.AutoSize = true;
+            this.PointsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PointsLabel.Font = new System.Drawing.Font("Montserrat", 13F, System.Drawing.FontStyle.Bold);
+            this.PointsLabel.Location = new System.Drawing.Point(-4, 2);
+            this.PointsLabel.Name = "PointsLabel";
+            this.PointsLabel.Size = new System.Drawing.Size(82, 25);
+            this.PointsLabel.TabIndex = 6;
+            this.PointsLabel.Text = "Points: ";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkCyan;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.Points);
+            this.panel1.Controls.Add(this.PointsLabel);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(159, 33);
+            this.panel1.TabIndex = 6;
+            // 
+            // Points
+            // 
+            this.Points.AutoSize = true;
+            this.Points.BackColor = System.Drawing.Color.Transparent;
+            this.Points.Font = new System.Drawing.Font("Montserrat", 13F, System.Drawing.FontStyle.Bold);
+            this.Points.Location = new System.Drawing.Point(68, 2);
+            this.Points.Name = "Points";
+            this.Points.Size = new System.Drawing.Size(24, 25);
+            this.Points.TabIndex = 7;
+            this.Points.Text = "0";
+            // 
+            // CurrentPointsLabel
+            // 
+            this.CurrentPointsLabel.AutoSize = true;
+            this.CurrentPointsLabel.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold);
+            this.CurrentPointsLabel.Location = new System.Drawing.Point(170, 77);
+            this.CurrentPointsLabel.Name = "CurrentPointsLabel";
+            this.CurrentPointsLabel.Size = new System.Drawing.Size(27, 29);
+            this.CurrentPointsLabel.TabIndex = 5;
+            this.CurrentPointsLabel.Text = "0";
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(840, 650);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.LosePanel);
+            this.Controls.Add(this.EnemyPictureBox2);
+            this.Controls.Add(this.EnemyPictureBox);
+            this.Controls.Add(this.CarPictureBox);
+            this.Controls.Add(this.MapPictureBox);
+            this.Controls.Add(this.MapPictureBox2);
+            this.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.Name = "GameForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fast and Furious";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GameForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CarPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MapPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MapPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyPictureBox2)).EndInit();
+            this.LosePanel.ResumeLayout(false);
+            this.LosePanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MapPictureBox;
+        private System.Windows.Forms.PictureBox CarPictureBox;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox MapPictureBox2;
+        private System.Windows.Forms.PictureBox EnemyPictureBox;
+        private System.Windows.Forms.PictureBox EnemyPictureBox2;
+        private System.Windows.Forms.Panel LosePanel;
+        private System.Windows.Forms.Label LoseLabel;
+        private System.Windows.Forms.Button RestartButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button HomeButton;
+        private System.Windows.Forms.Label PointsLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Points;
+        private System.Windows.Forms.Label CurrentPointsLabel;
+    }
+}
+
---
+++ Fasr-n-Furious/GameForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fasr_n_Furious
+{
+    public partial class GameForm : Form
+    {
+        Random rand = new Random();
+        private bool isPaused = false;
+        private int mapSpeed = 5;
+        private int mapHeight = 650;
+        private int points = 0;
+        private int enemy1TopLocation = -150;
+        private int enemy2TopLocation = -400;
+
+        public GameForm()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            
+            MapPictureBox.Top += mapSpeed;
+            MapPictureBox2.Top += mapSpeed;
+            EnemyPictureBox.Top += mapSpeed;
+            EnemyPictureBox2.Top += mapSpeed;
+
+            points += mapSpeed;
+            Points.Text = Convert.ToString(points);
+
+            if (mapSpeed < 20) mapSpeed ++;
+
+            if (MapPictureBox.Top >= mapHeight)
+            {
+                MapPictureBox.Top = 0;
+                MapPictureBox2.Top = -mapHeight;
+            }
+
+            if (EnemyPictureBox.Top >= mapHeight)
+            {
+                EnemyPictureBox.Top = enemy1TopLocation;
+                EnemyPictureBox.Left = rand.Next(150, 300);
+            }
+            if (EnemyPictureBox2.Top >= mapHeight)
+            {
+                EnemyPictureBox2.Top = enemy2TopLocation;
+                EnemyPictureBox2.Left = rand.Next(300, 560);
+            }
+
+            if (CarPictureBox.Bounds.IntersectsWith(EnemyPictureBox.Bounds) || CarPictureBox.Bounds.IntersectsWith(EnemyPictureBox2.Bounds))
+            {
+                timer.Stop();
+                LosePanel.Visible = true;
+                CurrentPointsLabel.Text = Convert.ToString(points);
+            }
+        }
+
+        private void GameForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            int carSpeed = 20;
+
+            //Car controls
+            if (!isPaused)
+            {
+                if ((e.KeyCode == Keys.Left || e.KeyCode == Keys.A) && CarPictureBox.Left >= 150)
+                {
+                    CarPictureBox.Left -= carSpeed;
+                }
+                else if ((e.KeyCode == Keys.Right || e.KeyCode == Keys.D) && CarPictureBox.Right <= 695)
+                {
+                    CarPictureBox.Left += carSpeed;
+                }
+                else if ((e.KeyCode == Keys.Down || e.KeyCode == Keys.S) && CarPictureBox.Top <= 560)
+                {
+                    CarPictureBox.Top += carSpeed;
+                }
+                else if ((e.KeyCode == Keys.Up || e.KeyCode == Keys.W) && CarPictureBox.Top >= 20)
+                {
+                    CarPictureBox.Top -= carSpeed;
+                }
+            }
+
+            //Handle game
+            if (e.KeyCode == Keys.P)
+            {
+                timer.Stop();
+                isPaused = true;
+            }
+            else if (e.KeyCode == Keys.R)
+            {
+                timer.Start();
+                isPaused = false;
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            EnemyPictureBox.Top = enemy1TopLocation;
+            EnemyPictureBox2.Top = enemy2TopLocation;
+            points = 0;
+            mapSpeed = 5;
+            LosePanel.Visible = false;
+            timer.Start();
+        }
+    }
+}
